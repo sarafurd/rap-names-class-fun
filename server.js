7057392d74f73d68edb80427c87b09168af9c12c
sara <sruthfd@csu.fullerton.edu>
2023-04-19 23:01:15 -0700

    third commit

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
 
 const rappers = {
     '21 savage': {
-        'age': 29,
+        'age': 30,
+        'children': 3,
         'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England'
+        'birthLocation': 'London, England, UK'
     },
     'chance the rapper': {
-        'age': 28,
+        'age': 30,
+        'children': 2,
         'birthName': 'Chancelor Bennett',
-        'birthLocation': 'Chicago, Illinois'
+        'birthLocation': 'Chicago, Illinois, USA'
     },
-    'dylan': {
-        'age': 28,
-        'birthName': 'Dylan',
-        'birthLocation': 'Dylan'
+    'cardi b': {
+        'age': 30,
+        'children': 2,
+        'birthName': 'Belcalis Marlenis Almánzar',
+        'birthLocation': 'New York City, New York, USA'
     },
+    'eminem': {
+        'age': 50,
+        'children': 1,
+        'birthName': 'Marshall Bruce Mathers III',
+        'birthLocation': 'St. Joseph, Missouri, USA'
+    }
 
 }
+
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
@@ -30,7 +44,7 @@ app.get('/api/:rapperName', (request, response) => {
     if (rappers[rappersName]) {
         response.json(rappers[rappersName])
     } else {
-        response.json(rappers['Dylan'])
+        response.json(rappers['cardi b'])
     }
 })
 
